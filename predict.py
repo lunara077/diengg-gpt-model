744166045769dc5c4bfc393f5ce7e46294cf7542
Lunara07 <49828654+Lunara07@users.noreply.github.com>
2022-02-18 09:13:33 +0200

    Update predict.py

--- predict.py
+++ predict.py
@@ -33,6 +33,7 @@ def predict(environ, start_response):
     
     #prediction = predict_image(model, image, inverted)
     prediction = {'anomalies': where(pred == -1)}
+    prediction = prediction.tolist()
     # The following line allows Valohai to track endpoint predictions
     # while the model is deployed. Here we remove the full predictions
     # details as we are only interested in tracking the rest of the results.
