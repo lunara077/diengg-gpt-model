6b5e867ff3ac0e9f1a4480a3dff798323318f361
Lunara07 <49828654+Lunara07@users.noreply.github.com>
2022-02-18 08:55:23 +0200

    anomaly detection

--- predict.py
+++ predict.py
@@ -1,34 +1,42 @@
 import json
 
 from PIL import Image
+from numpy import where
 from werkzeug.wrappers import Request, Response
-
+import pandas as pd
 from utils.image import predict_image, process_image
 from utils.model import load_model
-
+import pickle
 model = None
 
 
 def predict(environ, start_response):
-    # Load input image data from the HTTP request
+    # Load inputdata from the HTTP request
     request = Request(environ)
+    print(request)
     if not request.files:
         return Response('no file uploaded', 400)(environ, start_response)
-    image_file = next(request.files.values())
-    image, inverted = process_image(Image.open(image_file))
+    csv_file = next(request.files.values())
+    test = pd.read_csv(csv_file)
+    one_hot_encoded_data2 = pd.get_dummies(test, columns = ['Code'])
+    df2 = one_hot_encoded_data2[["Delay", "Code_200", "Code_201", "Code_204", "Code_302", "Code_400", "Code_404", "Code_500"]]
+    #image, inverted = process_image(Image.open(image_file))
 
     # The predictor must be lazily instantiated;
     # the TensorFlow graph can apparently not be shared
     # between processes.
     global model
     if not model:
-        model = load_model('model.h5')
-    prediction = predict_image(model, image, inverted)
-
+        with open('model.h5', 'rb') as f:
+            model = pickle.load(f)
+    pred = model.predict(df2)
+    
+    #prediction = predict_image(model, image, inverted)
+    prediction = {'anomalies': where(pred == -1)}
     # The following line allows Valohai to track endpoint predictions
     # while the model is deployed. Here we remove the full predictions
     # details as we are only interested in tracking the rest of the results.
-    print(json.dumps({'vh_metadata': {k: v for k, v in prediction.items() if k != 'predictions'}}))
+    #print(json.dumps({'vh_metadata': {k: v for k, v in prediction.items() if k != 'predictions'}}))
 
     # Return a JSON response
     response = Response(json.dumps(prediction), content_type='application/json')
--- train_model.py
+++ train_model.py
@@ -1,16 +1,18 @@
 import uuid
+import pickle
+
 
 import numpy as np
 import tensorflow as tf
 import valohai
-
-
-def log_metadata(epoch, logs):
-    """Helper function to log training metrics"""
-    with valohai.logger() as logger:
-        logger.log('epoch', epoch)
-        logger.log('accuracy', logs['accuracy'])
-        logger.log('loss', logs['loss'])
+import pandas as pd
+from sklearn.neighbors import LocalOutlierFactor
+# def log_metadata(epoch, logs):
+#     """Helper function to log training metrics"""
+#     with valohai.logger() as logger:
+#         logger.log('epoch', epoch)
+#         logger.log('accuracy', logs['accuracy'])
+#         logger.log('loss', logs['loss'])
 
 
 def main():
@@ -21,7 +23,7 @@ def main():
         step='train-model',
         image='tensorflow/tensorflow:2.6.0',
         default_inputs={
-            'dataset': 'https://valohaidemo.blob.core.windows.net/mnist/preprocessed_mnist.npz',
+            'dataset': 'http://www.testifytech.ml/Traffic_train.csv',
         },
         default_parameters={
             'learning_rate': 0.001,
@@ -32,38 +34,43 @@ def main():
     # Read input files from Valohai inputs directory
     # This enables Valohai to version your training data
     # and cache the data for quick experimentation
+    #print(valohai.inputs('dataset'))
+    data = pd.read_csv(valohai.inputs('dataset').path())
+    one_hot_encoded_data = pd.get_dummies(data, columns = ['Code'])
+    df = one_hot_encoded_data[["Delay", "Code_200", "Code_201", "Code_204", "Code_302", "Code_400", "Code_404", "Code_500"]]
+    model = LocalOutlierFactor(n_neighbors=20, novelty=True, contamination=0.1)
+    model.fit(df)
+    #input_path = valohai.inputs('dataset').path()
+    #with np.load(input_path, allow_pickle=True) as f:
+    #    x_train, y_train = f['x_train'], f['y_train']
+    #    x_test, y_test = f['x_test'], f['y_test']
 
-    input_path = valohai.inputs('dataset').path()
-    with np.load(input_path, allow_pickle=True) as f:
-        x_train, y_train = f['x_train'], f['y_train']
-        x_test, y_test = f['x_test'], f['y_test']
+    #model = tf.keras.models.Sequential([
+    #    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    #    tf.keras.layers.Dense(128, activation='relu'),
+    #     tf.keras.layers.Dropout(0.2),
+    #     tf.keras.layers.Dense(10),
+    # ])
 
-    model = tf.keras.models.Sequential([
-        tf.keras.layers.Flatten(input_shape=(28, 28)),
-        tf.keras.layers.Dense(128, activation='relu'),
-        tf.keras.layers.Dropout(0.2),
-        tf.keras.layers.Dense(10),
-    ])
-
-    optimizer = tf.keras.optimizers.Adam(learning_rate=valohai.parameters('learning_rate').value)
-    loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-    model.compile(optimizer=optimizer,
-                  loss=loss_fn,
-                  metrics=['accuracy'])
+    # optimizer = tf.keras.optimizers.Adam(learning_rate=valohai.parameters('learning_rate').value)
+    # loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+    # model.compile(optimizer=optimizer,
+    #               loss=loss_fn,
+    #               metrics=['accuracy'])
 
     # Print metrics out as JSON
     # This enables Valohai to version your metadata
     # and for you to use it to compare experiments
 
-    callback = tf.keras.callbacks.LambdaCallback(on_epoch_end=log_metadata)
-    model.fit(x_train, y_train, epochs=valohai.parameters('epochs').value, callbacks=[callback])
+    ##callback = tf.keras.callbacks.LambdaCallback(on_epoch_end=log_metadata)
+    #model.fit(x_train, y_train, epochs=valohai.parameters('epochs').value, callbacks=[callback])
 
     # Evaluate the model and print out the test metrics as JSON
 
-    test_loss, test_accuracy = model.evaluate(x_test,  y_test, verbose=2)
-    with valohai.logger() as logger:
-        logger.log('test_accuracy', test_accuracy)
-        logger.log('test_loss', test_loss)
+    #test_loss, test_accuracy = model.evaluate(x_test,  y_test, verbose=2)
+    # with valohai.logger() as logger:
+    #     logger.log('test_accuracy')
+    #     logger.log('test_loss')
 
     # Write output files to Valohai outputs directory
     # This enables Valohai to version your data
@@ -71,7 +78,9 @@ def main():
 
     suffix = uuid.uuid4()
     output_path = valohai.outputs().path(f'model-{suffix}.h5')
-    model.save(output_path)
+    with open(output_path, 'wb') as f:
+        pickle.dump(model, f)
+    #model.save(output_path)
 
 
 if __name__ == '__main__':
